900172ebbd4fb2a83428b92f24e27409883dfee7
chimbox <lfregoso@sahuarolabs.com>
2021-12-03 02:46:13 -0700

    Se obtienen los boletos y se muestran en el frontend

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app=express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, ()=>{
+    console.log('Servidor corriendo en el puerto 3000');
+});
